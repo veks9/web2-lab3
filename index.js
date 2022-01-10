6ac5b7be695c6a3fbb5dcd9ffa5fcb5473d22713
Vedran Hernaus <vhernaus@gmail.com>
2022-01-10 01:47:42 +0100

    bug fix

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.get("/recordCollection", function (req, res) {
   res.json(recordCollection);
 });
 
-app.listen(8888, "localhost", function () {
+const port = process.env.PORT || 8888
+app.listen(port, function () {
   console.log(`Server listening on http://${this.address().address}:${this.address().port}`);
 });
