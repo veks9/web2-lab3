4aeb1e001027a87d739d7cd9ea67b3e222340439
Vedran Hernaus <vhernaus@gmail.com>
2022-01-10 01:33:12 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+var cors = require("cors");
+const fs = require("fs");
+
+let recordCollection;
+fs.readFile("recordCollection.json", function (err, data) {
+  recordCollection = JSON.parse(data);
+
+  recordCollection = recordCollection.map(function (record, index) {
+    return {
+      id: index,
+      ...record,
+    };
+  })
+});
+
+const app = express();
+app.use(express.static('public'));
+app.use(cors());
+
+app.get("/recordCollection", function (req, res) {
+  res.json(recordCollection);
+});
+
+app.listen(8888, "localhost", function () {
+  console.log(`Server listening on http://${this.address().address}:${this.address().port}`);
+});
